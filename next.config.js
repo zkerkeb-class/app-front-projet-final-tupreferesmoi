ef7e1cb1a920073c45228a487eb32627a3434c11
Amidou D. <72388952+Kazuma746@users.noreply.github.com>
2025-02-03 21:47:01 +0100

    feat: mise à jour du frontend (#31)
    Co-authored-by: Amidou <amidou.diallo@perfectogroupe.fr>

--- next.config.js
+++ next.config.js
@@ -57,10 +57,10 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: "/api/:path*",
-                destination: "http://localhost:3000/api/:path*",
-            },
-        ];
+                source: '/api/:path*',
+                destination: 'http://localhost:3000/api/:path*'
+            }
+        ]
     },
 };
 
